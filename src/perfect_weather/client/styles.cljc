000d59140350616a8c76a0095fddb174220bc891
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-03-23 23:40:31 +0800

    Make it beautiful

--- src/perfect_weather/client/styles.cljc
+++ src/perfect_weather/client/styles.cljc
@@ -1,13 +1,51 @@
-(ns perfect-weather.client.styles)
+(ns perfect-weather.client.styles
+  (:require
+    [garden.stylesheet :refer [at-import]]))
+
+(def color-accent "#4cafef")
+(def color-grid-background "#f5f5f5")
+(def color-grid-border "#ddd")
+
+(def row 
+  [:&
+   {:display "flex"
+    :justify-content "center"}
+
+   [:>.legend
+    {:width "10rem"
+     :flex-shrink 0
+     :box-sizing "border-box"}]
+
+   [:>.main
+    {:width "100%"
+     :flex-grow 2
+     :min-width "25rem"
+     :max-width "50rem"}
+
+    [:>.columns
+     {:display "flex"
+      :height "100%"
+      :width "100%"}
+
+     [:>.column
+      {:width (str (float (/ 100 12)) "%")
+       :border-left [["1px" "solid" color-grid-border]]}
+
+      ["&:nth-child(even)"
+       {:background color-grid-background}]
+
+      [:&:last-child
+       {:border-right [["1px" "solid" color-grid-border]]}]]]]])
 
 (defn styles []
-  [
+  [(at-import "https://fonts.googleapis.com/css?family=Montserrat:400,600")
    [:body :h1 :h2 :p
     {:margin 0
      :padding 0}]
 
    [:#app
-    
+    {:font-family "Montserrat"}
+
     [:>.app
      {:display "flex"
       :min-height "100vh"
@@ -16,15 +54,81 @@
       :align-items "center"}
 
      [:>form
-      {:font-size "2rem"}
+      {:font-size "1.5rem"
+       :margin "3rem"}
 
       [:>input
-        {:font-size "2rem"
-         :width "8em"
-         :padding "0.25rem"}]]
-     
+       {:font-size "1.5rem"
+        :width "8em"
+        :padding "0.25rem"}]]
+
      [:>.results
-      
-      [:>.result
-       {:display "flex"
-        :margin "2em 0"}]]]]])
+      ; fudge to visually center
+      {:margin-right "4rem"
+       :width "100%"}
+
+      (let [height "4rem"]
+        [:>.result
+         row
+         {:height height}
+
+         [:>.legend
+          {:text-transform "uppercase"
+           :height height
+           :line-height height
+           :padding-right "0.5rem"
+           :font-size "0.85rem"
+           :letter-spacing "0.1em"
+           :text-align "right"
+           :font-weight "bold"
+           :color color-accent}
+
+          [:&:first-letter
+           {:font-size "1.15em"}]]
+
+         [:>.calendar
+
+          [:>.columns
+           {:margin-bottom (str "-" height)} 
+
+           [:>.month]]
+
+          [:>.ranges
+           {:display "flex"
+            :height "100%" 
+            :align-items "center"}
+
+           [:>.range
+            {:height "1em"}
+
+            [:&.never
+             {:color color-accent
+              :width "100%"
+              :font-weight "bold"
+              :text-align "center"}]
+
+            [:&.fill
+             {:background color-accent
+              :border-radius "1em"}]
+
+            [:&.start
+             {:border-radius "0 1em 1em 0"}]
+
+            [:&.end
+             {:border-radius "1em 0 0 1em"}]]]]])
+
+      (let [height "2rem"]
+        [:>.bottom-legend
+         row
+         {:height height}
+
+         [:>.main
+
+          [:>.columns
+
+           [:>.column
+            {:line-height height
+             :text-align "center"
+             :text-transform "uppercase"
+             :font-size "0.7em"
+             :letter-spacing "0.1em"}]]]])]]]])
--- src/perfect_weather/client/views/app.cljs
+++ src/perfect_weather/client/views/app.cljs
@@ -5,58 +5,36 @@
     [perfect-weather.data.months :refer [months months-abbr]]))
 
 (defn calendar-view [ranges]
-  [:div.calendar {:style {:width "80vw"}}
+  [:div.calendar.main 
+   (into
+     [:div.columns]
+     (repeat 12 [:div.column]))
    (into 
-     [:div.ranges {:style {:display "flex"
-                           :margin-bottom "-2.5em"
-                           :min-height "3em"}}]
+     [:div.ranges]
      (if (seq ranges)
-       (for [[width text] (->> (concat [{:text nil :range [0 0]}] ranges [{:text nil :range [365 365]}])
-                               (partition 2 1)
-                               (mapcat (fn [[{[start end] :range text :text} {[next-start _] :range}]]
-                                         [[(- end start) text]
-                                          [(- next-start end) nil]]))
-                               rest
-                               butlast)]
-         [:div.range {:style {:width (str (* 100 (/ width 365)) "%")
-                              :display "flex"
-                              :flex-direction "column"
-                              :align-items "center"}}
-          [:div.text {:style {:text-align "center"
-                              :white-space "nowrap"}}
-           text]
-          [:div.bar {:style {:height "2em"
-                             :width "100%"
-                             :margin "0.5em 0"
-                             :border-radius "5px"
-                             :background (when text "green")}}]])
-       [[:div.range {:style {:width "100%"
-                             :display "flex"
-                             :flex-direction "column"
-                             :align-items "center"}}
-         ":( Never"]]))
-   (into 
-     [:div.months {:style {:display "flex"}}]
-     (for [month months-abbr]
-       [:div.month {:style {:width (str (/ 100 12) "%")
-                            :border-left "1px solid #ccc"
-                            :border-right (when (= month (last months-abbr))
-                                            "1px solid #ccc")
-                            :text-align "center"
-                            :padding-top "2.5em"
-                            :z-index -1
-                            :box-size "border-box"}} month]))])
+       (for [[start end text] 
+             (->> (concat [{:text nil :range [0 0]}] 
+                          ranges 
+                          [{:text nil :range [365 365]}])
+                  (partition 2 1)
+                  (mapcat (fn [[{[start end] :range text :text} {[next-start _] :range}]]
+                            [[start end text]
+                             [end next-start nil]]))
+                  rest
+                  butlast)]
+         [:div.range {:class [(when (= start 0)
+                                "start")
+                              (when (= end 365)
+                                "end")
+                              (when text "fill")]
+                      :title text
+                      :style {:width (str (* 100 (/ (- end start) 365)) "%")}}])
+       [[:div.range.never "☹"]]))])
 
 (defn result-view [result]
- [:div.result
-  [:div.meta
-   [:div.location
-    [:div.city
-     (result :city) ", "]
-    [:div.country 
-     (result :country)]]
-   [:div.stats
-    (result :percent) "% 👌"]]
+ [:div.result.row
+  [:div.legend
+   (result :city)]
   [calendar-view (result :ranges)]])
 
 (defn results-view []
@@ -64,11 +42,19 @@
    (doall
      (for [result @(subscribe [:results])]
        ^{:key (result :city)}
-       [result-view result]))])
+       [result-view result]))
+   
+   [:div.row.bottom-legend
+    [:div.legend]
+    [:div.main
+     (into 
+       [:div.columns]
+       (for [month months-abbr]
+         [:div.column month]))]]])
 
 (defn form-view []
   [:form
-   "When is the weather nice in "
+   "When's the weather nice in "
    [:input {:value @(subscribe [:query])
             :on-change (fn [e]
                          (dispatch [:update-query! (.. e -target -value)]))
