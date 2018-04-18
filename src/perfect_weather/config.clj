751312746448db1ab75665c5b5320c248c4311a6
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-04-18 17:15:01 +0800

    Set the default page title

--- src/perfect_weather/config.clj
+++ src/perfect_weather/config.clj
@@ -1,5 +1,6 @@
 (ns perfect-weather.config)
 
 (def config 
-  {:css {:styles "perfect-weather.client.ui.app-styles/styles"}
+  {:title "Sunny Pursuits"
+   :css {:styles "perfect-weather.client.ui.app-styles/styles"}
    :cljs {:main "perfect-weather.client.core"}})
